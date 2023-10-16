3012765c54f99760aa5e81ab4eac1fa7641fe8be
jsrisairam <2000030954@kluniversity.in>
2023-10-16 22:37:47 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
 
+const Computerchoicedisplay = document.getElementById('Computerchoicedisplay');
+const userchoicedisplay = document.getElementById('userchoicedisplay');
+const resultdisplay = document.getElementById('resultdisplay');
+const possibleChoice = document.querySelectorAll('button');
+let userchoice
+let computerchoice
+let result
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  possibleChoice.forEach(possibleChoice => possibleChoice.addEventListener('click',(e) =>{
+    userchoice=e.target.id
+    userchoicedisplay.innerHTML = userchoice;
+    generatecomputerchoice();
+    getresult();
+  }));
+  
 }
 
+function generatecomputerchoice() {
+  const randomnumber = Math.floor(Math.random()*3)+1;
+  if(randomnumber ==1)
+  {
+    computerchoice = 'rock';
+  }
+  if(randomnumber == 2)
+  {
+    computerchoice = 'scissors';
+
+  }
+  if(randomnumber == 3)
+  {
+    computerchoice = 'paper';
+  }
+    Computerchoicedisplay.innerHTML= computerchoice;
+}
+
+
+function getresult()
+{
+  if(computerchoice === userchoice)
+  {
+    result = 'its a draw'
+  }
+  if(computerchoice == 'rock'  &&  userchoice =='paper')
+  {
+    result = 'you win!'
+  }
+  if(computerchoice == 'paper'  &&  userchoice =='scissor')
+  {
+    result = 'you win!'
+  }
+  if(computerchoice == 'rock'  &&  userchoice =='scissor')
+  {
+    result = 'you lost!'
+  }
+  
+  resultdisplay.innerHTML=result;
+}
 export default App;
